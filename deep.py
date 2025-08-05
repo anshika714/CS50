710b4e00eee73677473ad8689e840ed954591b96
Anshika Gupta <anshikaa.0714@gmail.com>
2025-08-05 23:07:31 +0530

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,6 @@
+ans=input("What is the answer to the great question of life,the universe,and everything?").strip().lower()
+if ans=="42" or ans=="forty-two" or ans=="forty two":
+    print("Yes")
+else:
+    print("No")
+
